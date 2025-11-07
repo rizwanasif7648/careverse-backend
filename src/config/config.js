f112b37788e3e72ba5f1ffe51259c6951321abd0
Rizwan <rizwan@clustox.com>
2025-11-07 18:12:05 +0500

    Allow all origin

--- src/config/config.js
+++ src/config/config.js
@@ -63,7 +63,7 @@ module.exports = {
 
   // CORS Configuration
   cors: {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: true, // Allow all origins for now
     credentials: true
   },
 
